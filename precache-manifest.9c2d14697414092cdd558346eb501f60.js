f3f059da7890344890184fd71054b13c83ecca45
dmitry19942 <gorustovich2013@gmail.com>
2021-11-07 10:30:17 +0300

    Updates

---
+++ precache-manifest.9c2d14697414092cdd558346eb501f60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18edb992beada76acb129fcc9300b15d",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "2cebd1b9ed3a93556cba",
+    "url": "/todolist/static/css/main.d780d2b8.chunk.css"
+  },
+  {
+    "revision": "7c10a9405fd26de1d386",
+    "url": "/todolist/static/js/2.9e141c36.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todolist/static/js/2.9e141c36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cebd1b9ed3a93556cba",
+    "url": "/todolist/static/js/main.4f8be1bb.chunk.js"
+  },
+  {
+    "revision": "197f51c8578d731c6831",
+    "url": "/todolist/static/js/runtime-main.e7725399.js"
+  }
+]);
